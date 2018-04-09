03fe05612504a9c8b039ff6eeb0f6358aa937efc
shahzad babar <shzad78@gmail.com>
2018-04-10 00:29:53 +0500

    skelton

---
+++ src/js/components/practice.jsx
@@ -0,0 +1,2 @@
+import React, {Component} from 'react';
+imoprt ProtoTypes from 'proto-types';
--- src/js/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import counter from './counter'
-
-const reducers = combineReducers({
-  counter: counter
-})
-
-export default reducers;
-
